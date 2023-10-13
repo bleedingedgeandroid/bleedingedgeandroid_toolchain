0bf3bb301ff1f076731573351c61a4b7caf48bbb
PugzAreCute <me@pugzarecute.com>
2023-10-13 19:24:36 +0530

    toolchain: first scripts

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "MUST BE RUN FROM PROJECT ROOT. CTRL+C NOW IF NOT BEING RUN FROM THERE"
+echo "giving 10s grace time"
+
+sleep 10
+
+chmod +x build/envsetup.sh
+source build/envsetup.sh
+lunch gsi_arm64-userdebug
+repo init https://github.com/bleedingedgeandroid/manifest -b udc
+repo sync
+m -j32
